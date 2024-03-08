68af774329a4e6e9943a56fedec19ad8059472e8
Mouad Kimdil <mkimdil@e3r9p18.1337.ma>
2024-03-08 10:49:33 +0100

    problem ctrl + D fixed

--- bonus_helpers.c
+++ bonus_helpers.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   bonus_helpers.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: asel-kha <asel-kha@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/23 01:50:25 by asel-kha          #+#    #+#             */
-/*   Updated: 2024/03/07 20:35:09 by asel-kha         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "pipex.h"
 
@@ -17,23 +6,13 @@ void	init_struct(t_pipex *data, char **av, int ac)
 	if (data->heredoc == 1)
 		heredoc(data, av[2]);
 	else
-	{
 		data->ifile = open(av[1], O_RDONLY, 0644);
-		if (data->ifile == -1)
-			err();
-	}
 	if (data->heredoc == 1)
-	{
 		data->ofile = open(av[ac - 1], O_CREAT | O_APPEND | O_RDWR, 0644);
-		if (data->ofile == -1)
-			err();
-	}
 	else
-	{
 		data->ofile = open(av[ac - 1], O_CREAT | O_TRUNC | O_RDWR, 0644);
-		if (data->ofile == -1)
-			err();
-	}
+	if (data->ifile < 0 || data->ofile < 0)
+		err();
 }
 
 void	free_struct(t_pipex *data)
